bc2907bf34030719b9dac1292816561bdd491532
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-07-01 13:37:53 -0700

    styles

--- dist/resume-7bc5512895a52d5f.js
+++ dist/resume-7bc5512895a52d5f.js
@@ -633,7 +633,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('resume-1897fca87706e96d_bg.wasm', import.meta.url);
+        input = new URL('resume-7bc5512895a52d5f_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,17 @@
 use yew::prelude::*;
 
 #[function_component(App)]
-fn app()->Html{
+fn app() -> Html {
+    html! {
+        <body >
+        <h1 style="color:blue;">{"Hello World!"}</h1>
+        <b>{"this is hello world"}</b>
+        </body>
 
-html!{
-    <body>
-    <h1>{"Hello World!"}</h1>
-    <b>{"this is hello world"}</b>
-    </body>
-   
+    }
 }
-
-}
-
-
-
-
-
-
-
-
-
 
 fn main() {
-    let app =     yew::Renderer::<App>::new();
+    let app = yew::Renderer::<App>::new();
     app.render();
 }
