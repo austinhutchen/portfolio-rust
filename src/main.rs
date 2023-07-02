009d0e8df9a52a9492399aa6fa4e0ad9705414f1
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-07-02 08:11:43 -0700

    changes

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,18 @@ use yew::prelude::*;
 // http://127.0.0.1:8080
 #[function_component(App)]
 fn app() -> Html {
+    let input_value_handle = use_state(String::default);
+    let input_value = (*input_value_handle).clone();
+
+
     html! {
         <html>
 
         <div class={classes!("center")}>
         <h1 ><b><u>{"Hello World!"}</u></b></h1>
-        <a href="" class={classes!("center")}>{"Welcome to my page!"}</a>
+        <a href="" class={classes!("unify")}>{"Welcome to my page!"}</a>
         <ul class={classes!("landing")} >
-       <li> <a href=""> <b>{"About me!"} </b> </a>  </li>
-       <li> <a href=""> <b>{"projects"} </b> </a> </li>
-       <li> <a href=""> <b> {"resume"} </b> </a>  </li>
-       <li> <a href=""> <b>{"more"} </b> </a>  </li>
+
         </ul>
         </div>
         </html>
