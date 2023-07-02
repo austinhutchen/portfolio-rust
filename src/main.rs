71c202ec123dd49852e709d1a81ed6f820f9f29e
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-07-02 08:51:00 -0700

    cargo web integration

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use wasm_bindgen::prelude::*;
 // http://127.0.0.1:8080
 
 
+
+
 #[wasm_bindgen(module = "/src/components/helpers.js")]
 extern "C" {
     fn add(a: u32, b: u32) -> u32;
@@ -18,7 +20,7 @@ fn app() -> Html {
         <h1 ><b><u>{"Hello World!"}</u></b></h1>
         <a href="" class={classes!("unify")}>{"Welcome to my page!"}</a>
         <ul class={classes!("landing")} >
-       <li> <button onclick="'location.href=./views/about.html'">  </button> </li>
+       <li> <button onclick="document.location = ./views/about.html">  </button> </li>
        <li> <a href=""> <b>{"projects"} </b> </a> </li>
        <li> <a href=""> <b> {"resume"} </b> </a>  </li>
        <li> <a href=""> <b>{"more"} </b> </a>  </li>
