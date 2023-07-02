4eef71bdaf11507c9538f93699d098921524c399
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-07-02 08:34:31 -0700

    js integrtation setup

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
 use yew::prelude::*;
+use wasm_bindgen::prelude::*;
 // http://127.0.0.1:8080
+
+
+#[wasm_bindgen(module = "/src/components/helpers.js")]
+extern "C" {
+    fn add(a: u32, b: u32) -> u32;
+}
+
+
 #[function_component(App)]
 fn app() -> Html {
-    let input_value_handle = use_state(String::default);
-    let input_value = (*input_value_handle).clone();
-
-
     html! {
         <html>
 
         <div class={classes!("center")}>
-        <h1 class={classes!("heading")} ><b><u>{"Hello World!"}</u></b></h1>
+        <h1 ><b><u>{"Hello World!"}</u></b></h1>
         <a href="" class={classes!("unify")}>{"Welcome to my page!"}</a>
         <ul class={classes!("landing")} >
-
+       <li> <button onclick="'location.href=./views/about.html'">  </button> </li>
+       <li> <a href=""> <b>{"projects"} </b> </a> </li>
+       <li> <a href=""> <b> {"resume"} </b> </a>  </li>
+       <li> <a href=""> <b>{"more"} </b> </a>  </li>
         </ul>
         </div>
         </html>
